013ab7caae6ebdcd0096f1b343d4af1720070a59
superquan <2414375340@qq.com>
2021-12-30 20:21:04 +0800

    master first commit

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class HelloWrold {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+	System.out.println("hello master!");
+	System.ont.println("hello superquan!");
+    }
+}
